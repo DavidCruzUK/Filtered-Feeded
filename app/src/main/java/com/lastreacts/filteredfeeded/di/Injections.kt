97251965839c2ce8a4d506fbaff1c58a3951e719
David Cruz Anaya <me@davidcruz.co.uk>
2020-05-10 15:34:13 +0100

    Remove Unused injection in to the activity for unused

--- app/src/main/java/com/lastreacts/filteredfeeded/di/Injections.kt
+++ app/src/main/java/com/lastreacts/filteredfeeded/di/Injections.kt
@@ -1,15 +1,10 @@
 package com.lastreacts.filteredfeeded.di
 
-import com.lastreacts.filteredfeeded.ui.activities.MainActivity
 import com.lastreacts.filteredfeeded.ui.fragments.MainFragment
 import com.lastreacts.filteredfeeded.ui.fragments.TweetsListFragment
 
 interface Injections {
 
-    // Activities
-    fun inject(activity: MainActivity)
-
-    // Fragments
     fun inject(fragment: MainFragment)
     fun inject(fragment: TweetsListFragment)
 
--- app/src/main/java/com/lastreacts/filteredfeeded/ui/activities/MainActivity.kt
+++ app/src/main/java/com/lastreacts/filteredfeeded/ui/activities/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.lastreacts.filteredfeeded.ui.activities
 
-import android.os.Bundle
 import androidx.navigation.Navigation
 import com.lastreacts.filteredfeeded.R
 import com.lastreacts.filteredfeeded.ui.base.BaseActivity
 
 class MainActivity : BaseActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        getAppComponent().inject(this)
-    }
 
     override fun layoutRes(): Int = R.layout.activity_main
 
